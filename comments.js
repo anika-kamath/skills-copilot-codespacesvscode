033f7af9cfaf78e0114efe93aa4ece88599fe3dc
Anika Kamath <87904385+anika-kamath@users.noreply.github.com>
2023-08-07 07:54:47 +0000

    copilot commit 3

---
+++ comments.js
@@ -0,0 +1,14 @@
+//create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set up a route to handle the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
